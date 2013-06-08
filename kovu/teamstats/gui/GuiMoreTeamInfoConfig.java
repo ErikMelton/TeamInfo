b8689890d433c39f7d1125d204ada2ab2599c940
Erik Melton <mudmander@gmail.com>
2013-06-07 20:33:23 -0700

    Config

--- kovu/teamstats/gui/GuiMoreTeamInfoConfig.java
+++ kovu/teamstats/gui/GuiMoreTeamInfoConfig.java
@@ -24,13 +24,14 @@ public class GuiMoreTeamInfoConfig extends GuiScreen {
     public void initGui() {
         buttonList.clear();
         
-        buttonList.add(new GuiButton(0, width / 2 - 50, height / 2 + 5, 100, 20, (new StringBuilder()).append("Version: ").append(Kovu.isVersion ? "On" : "Off").toString()));
+        
+        buttonList.add(new GuiButton(1, width / 2 - 50, height / 2 + 5, 100, 20, (new StringBuilder()).append("Version: ").append(Kovu.isVersion ? "On" : "Off").toString()));
         buttonList.add(new GuiButton(6, width / 2 + 55, height / 2 + 5 + 24, 100, 20, "Back"));
     }
 
     public void actionPerformed(GuiButton guibutton) {
     	
-        if (guibutton.id == 0) {
+        if (guibutton.id == 1) {
             Kovu.isVersion = !Kovu.isVersion;
             guibutton.displayString = (new StringBuilder()).append("Version: ").append(Kovu.isVersion ? "On" : "Off").toString();
         }
