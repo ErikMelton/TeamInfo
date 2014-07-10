7126e6ce77e012e5e818628b3b808469c49970e8
Erik Melton <mudmander@gmail.com>
2014-07-10 10:28:15 -0700

    Removed Excess KeyBinding

--- src/main/java/kovu/teaminfo/util/KeyBindings.java
+++ src/main/java/kovu/teaminfo/util/KeyBindings.java
@@ -9,16 +9,13 @@ public class KeyBindings
 {
 	public static KeyBinding mainGui;
 	public static KeyBinding ddGui;
-	public static KeyBinding configGui;
 	
 	public void init()
 	{
 		mainGui = new KeyBinding("Main GUI", Keyboard.KEY_SEMICOLON, "TeamInfo");
 		ddGui = new KeyBinding("Drag and Drop GUI", Keyboard.KEY_P, "TeamInfo");
-		configGui = new KeyBinding("Config GUI", Keyboard.KEY_LBRACKET, "TeamInfo");
 
 		ClientRegistry.registerKeyBinding(mainGui);
 		ClientRegistry.registerKeyBinding(ddGui);
-		ClientRegistry.registerKeyBinding(configGui);
 	}
 }
